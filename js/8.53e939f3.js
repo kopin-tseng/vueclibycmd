b8b5890d59bfc620769dfc86fff9cd977960c9c6
Nozomi <>
2022-11-19 15:54:03 +0800

    deploy

---
+++ js/8.53e939f3.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvueclibycmd"]=self["webpackChunkvueclibycmd"]||[]).push([[8],{9008:function(n,u,c){c.r(u),c.d(u,{default:function(){return o}});var e=c(3396);const r={class:"about"},t=(0,e._)("h1",null,"This is an product page",-1),s=[t];function a(n,u){return(0,e.wg)(),(0,e.iD)("div",r,s)}var i=c(89);const l={},d=(0,i.Z)(l,[["render",a]]);var o=d}}]);
+//# sourceMappingURL=8.53e939f3.js.map
